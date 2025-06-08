3493ff19eb40fda43dcea9b037f6ea9e259ac378
Peter Ayad <eng.peter2013@gmail.com>
2025-06-08 07:35:57 -0700

    Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,13 +13,13 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter item name to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print("Item added successfully")
             pass
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter item name to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list: 
                 shopping_list.remove(item)
                 print("Item removed successfully") 
